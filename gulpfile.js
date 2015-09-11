27f624235ee1ffd9ee6e11684651a0f2c45d377e
dbautistav <david@opi.la>
2015-09-11 16:19:48 -0500

    Adding gulp tasks // Adding useful packages (dependencies)

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,35 @@
-var gulp = require('gulp');
+//var gulp = require('gulp');
+//
+//gulp.task('default', function () {
+//    console.log("Allo' Allo' World!");
+//});
 
-gulp.task('default', function() {
-    console.log("Allo' Allo' World!");
+/**
+ *  Welcome to your gulpfile!
+ *  The gulp tasks are splitted in several files in the gulp directory
+ *  because putting all here was really too long
+ */
+
+'use strict';
+
+var gulp = require('gulp');
+var wrench = require('wrench');
+
+/**
+ *  This will load all js or coffee files in the gulp directory
+ *  in order to load all gulp tasks
+ */
+wrench.readdirSyncRecursive('./gulp').filter(function (file) {
+  return (/\.(js|coffee)$/i).test(file);
+}).map(function (file) {
+  require('./gulp/' + file);
+});
+
+
+/**
+ *  Default task clean temporaries directories and launch the
+ *  main optimization build task
+ */
+gulp.task('default', ['clean'], function () {
+  gulp.start('serve');
 });
