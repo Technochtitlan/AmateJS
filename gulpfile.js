aaaee345058c5f905ee60c35d71995fec4f5fb1b
dbautistav <david@opi.la>
2015-09-11 17:07:21 -0500

    Adding ESLint // New gulp tasks added // New packages installed

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,4 @@
-//var gulp = require('gulp');
-//
-//gulp.task('default', function () {
-//    console.log("Allo' Allo' World!");
-//});
+/*eslint-env node*/
 
 /**
  *  Welcome to your gulpfile!
@@ -10,26 +6,28 @@
  *  because putting all here was really too long
  */
 
-'use strict';
+"use strict";
 
-var gulp = require('gulp');
-var wrench = require('wrench');
+var gulp = require("gulp");
+var gutil = require("gulp-util");
+//var wrench = require("wrench");
 
-/**
- *  This will load all js or coffee files in the gulp directory
- *  in order to load all gulp tasks
- */
-wrench.readdirSyncRecursive('./gulp').filter(function (file) {
-  return (/\.(js|coffee)$/i).test(file);
-}).map(function (file) {
-  require('./gulp/' + file);
-});
+///**
+// *  This will load all js or coffee files in the gulp directory
+// *  in order to load all gulp tasks
+// */
+//wrench.readdirSyncRecursive("./gulp").filter(function (file) {
+//  return (/\.(js|coffee)$/i).test(file);
+//}).map(function (file) {
+//  require("./gulp/" + file);
+//});
 
 
 /**
  *  Default task clean temporaries directories and launch the
  *  main optimization build task
  */
-gulp.task('default', ['clean'], function () {
-  gulp.start('serve');
+gulp.task("default", [], function () {
+  //gulp.start("serve");
+  gutil.log("Allo' Allo' World!");
 });
